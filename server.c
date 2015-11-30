3a909461f92ffe70e13d7976bfa14c99c5b7bab7
dwj300 <dwj300@gmail.com>
2015-11-30 13:42:11 -0500

    File server done! (I think?)

--- server.c
+++ server.c
@@ -1,44 +1,97 @@
 #include "server.h"
 
-void error(char *msg)
-{
+void error(char *msg) {
     perror(msg);
     exit(1);
 }
 
-void create_block(){}
+int create_block(char* block_id) {
+    FILE *file = fopen(block_id, "w+");
+    if (file == NULL) {
+        fprintf(stderr, "cannot create file\n");
+        return -1;
+    }
+    fclose(file);
+    return 0;
+}
 
-void update_block(){}
-
-//READ [id]
-void read_block(int socket_fd, char* block_id)
-{
-    FILE *file = fopen(block_id, "r");
-    if (file == NULL)
-    {
+int write_block(char *block_id, char *data){
+    FILE *file = fopen(block_id, "w");
+    if (file == NULL) {
         fprintf(stderr, "file does not exist\n");
-        return;
+        return -1;
+    }
+    fwrite(data, 1, 1024, file);
+    fclose(file);
+    return 0;
+}
+
+int read_block(int socket_fd, char* block_id) {
+    FILE *file = fopen(block_id, "r");
+    if (file == NULL) {
+        fprintf(stderr, "file does not exist\n");
+        return -1;
     }
     char buffer[1024];
     fread(buffer, 1, 1024, file);
     int n = write(socket_fd, buffer, 1024);
-    if (n < 0)
-    {
-        fprintf(stderr, "error writing to socket");   
+    if (n < 0) {
+        fprintf(stderr, "error writing to socket\n");
+        return -1;
     }
+    fclose(file);
+    return 0;
 }
 
-void delete_block(){}
+int delete_block(char *block_id){
+    int n = remove(block_id);
+    if (n < 0) {
+        fprintf(stderr, "Error deleting file\n");
+        return -1;
+    }
+    return 0;
+}
+
+int parse_args(char *buffer, char **opcode, char **block_id, char **data) {
+    char *space = strchr(buffer, ' ');
+    *opcode = malloc(sizeof(char) * 10);
+    strncpy(*opcode, buffer, (space - buffer));
+    *block_id = space + sizeof(char);
+
+    if (space == NULL) {
+        fprintf(stderr, "request not formatted correctly\n");
+        return -1;
+    }
+    // fprintf(stderr, "block id:%sd\n", block_id);
+    char *last_space = strchr(*block_id, ' ');
+    if (last_space == NULL) { // No data
+        size_t ln = strlen(*block_id) - 1;
+        if ((*block_id)[ln] == '\n') {
+            (*block_id)[ln] = '\0';
+        }
+    }
+    else {
+        (*data) = malloc(sizeof(char)*1024);
+        char *data_start = last_space + sizeof(char);
+        *last_space = '\0';
+        memcpy((*data), data_start, 1024);
+    }
+    return 0;
+}
 
 
 int main(int argc, char *argv[])
 {
-    int sockfd, newsockfd, port, clilen, n;
-    if (argc < 3)
-        error("usage: server [port] [directory]");
-    port = atoi(argv[1]);
-    char *directory = argv[2];
-
+    int sockfd, newsockfd, port, n;
+    socklen_t clilen;
+    if (argc < 2) {
+        //error("usage: server [port] [directory]");
+        port = 9995;
+    }
+    else {
+        port = atoi(argv[1]);
+        //char *directory = argv[2];
+    }
     printf("File server starting on port: %d\n", port);
 
     char buffer[2048];
@@ -46,81 +99,59 @@ int main(int argc, char *argv[])
     struct sockaddr_in serv_addr, cli_addr;
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0)
+    if (sockfd < 0) {
         error("ERROR opening socket");
+    }
     bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
     serv_addr.sin_port = htons(port);
-    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
-          error("ERROR on binding");
+    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+        error("ERROR on binding");
+    }
 
     listen(sockfd,5);
     clilen = sizeof(cli_addr);
 
-    while(1)
-    {
+    while(1) {
         newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-        if (newsockfd < 0)
+        if (newsockfd < 0) {
           error("ERROR on accept");
+        }
         bzero(buffer,256);
         n = read(newsockfd,buffer,2048);
-        if (n < 0)
+        if (n < 0) {
             error("ERROR reading from socket");
+        }
 
-        printf("Here is the message: %s\n",buffer);
-            //n = write(newsockfd,"I got your message",18);
+        // fprintf("Here is the message: %s\n",buffer);
         
-        char *space = strchr(buffer, ' ');
-        char opcode[10];
-        strncpy(opcode, buffer, (space - buffer));
-        space += sizeof(char);
-
-        if (space == NULL)
-        {
-            fprintf(stderr, "request not formatted correctly\n");
+        char *opcode = NULL;
+        char *block_id = NULL;
+        char *data = NULL;
+        if (parse_args(buffer, &opcode, &block_id, &data) < 0) {
             continue;
         }
-        if (strcmp(opcode, "CREATE") == 0)
-        {
+        
+        // fprintf(stderr, "opcode:%sdone\n", opcode);
+        // fprintf(stderr, "block_id:%sdone\n", block_id);
+        // fprintf(stderr, "data:%sdone\n", data);
 
+        if (strcmp(opcode, "CREATE") == 0) {
+            create_block(block_id);
         }
-        else if (strcmp(opcode, "READ") == 0)
-        {
-            /*char *next_space = strchr(space, ' ');
-            fprintf(stderr, next_space);
-            if (next_space == NULL)
-            {
-                fprintf(stderr, "request not formatted correctly\n");
-                continue;
-            }
-            char block_id[20];
-            strncpy(block_id, space, (next_space - space));
-            fprintf(stderr, "id:%sg\n", block_id);*/
-            //int block_id = atoi(space);
-
-            fprintf(stderr, "block id:%sd\n", space);
-            size_t ln = strlen(space) - 1;
-            if (space[ln] == '\n')
-                space[ln] = '\0';
-            read_block(newsockfd, space);
+        else if (strcmp(opcode, "READ") == 0) {
+            read_block(newsockfd, block_id);
         }
-        else if (strcmp(opcode, "UPDATE") == 0)
-        {
-            
+        else if (strcmp(opcode, "WRITE") == 0) {
+            write_block(block_id, data);
         }
-        else if (strcmp(opcode, "DELETE") == 0)
-        {
-            
+        else if (strcmp(opcode, "DELETE") == 0) {
+            delete_block(block_id);   
         }
-        else
-        {
+        else {
             fprintf(stderr, "incorrect opcode");
-        }   
-        fprintf(stderr, "opcode: %s\n", opcode);
-
-        if (n < 0)
-            error("ERROR writing to socket");
+        }
     }
     return 0;
 }
