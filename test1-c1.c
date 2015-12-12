2a7796aca16ec0daeb1a27a7fc9dd85d5e795354
dwj300 <dwj300@gmail.com>
2015-12-12 18:05:40 -0500

    testing

--- test1-c1.c
+++ test1-c1.c
@@ -1,5 +1,3 @@
-#define _XOPEN_SOURCE 500
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -20,54 +18,54 @@
 
 int main(int argc, char *argv[])
 {
-    int ifdes, fdes;
-    int err_value;
-    char* input_fname = "tocopy.txt";
-    char *buf;
-    int cache_hit;
-    //ssize_t nread;
+  int ifdes, fdes;
+  int err_value;
+  char input_fname[20];
+  char *buf;
+  int cache_hit;
+  ssize_t nread;
+  
+
+  
+  // the command line arguments include an input filename
+  if (argc < 2)
+    {
+      printf("usage: a.out <input filename>\n");
+      exit(0);
+    }
 
     // Initialize the client
-    initialize(argc, argv);
+  initialize(argc-1, argv + sizeof(char*));
 
-    // the command line arguments include an input filename
-    /*if (argc < 2)
-    {
-        printf("usage: a.out <input filename>\n");
-        exit(0);
-    }*/
-    //strcpy(input_fname, argv[1]);
-    ifdes = open(input_fname, O_RDONLY);
-    buf = (char *)malloc(5*ONEKB);
-    //nread = pread(ifdes, (void *)buf, 4*ONEKB,0);
-    pread(ifdes, (void *)buf, 4*ONEKB,0);
 
-    // create a file only once, say at client 1
-    err_value = pfs_create("pfs_file1", 3);
-    if(err_value < 0)
+  strcpy(input_fname, argv[1]);
+  ifdes = open(input_fname, O_RDONLY);
+  buf = (char *)malloc(5*ONEKB);
+  nread = pread(ifdes, (void *)buf, 4*ONEKB,0);
+
+  // create a file only once, say at client 1 
+  err_value = pfs_create("pfs_file1", 3);
+  if(err_value < 0)
     {
-        printf("Unable to create a file\n");
-        exit(0);
+      printf("Unable to create a file\n");
+      exit(0);
     }
 
-    // All the clients open the file
-    fdes = pfs_open("pfs_file1", 'w');
-    if(fdes < 0)
+  // All the clients open the file 
+  fdes = pfs_open("pfs_file1", 'w');
+  if(fdes < 0)
     {
-        printf("Error opening file\n");
-        exit(0);
+      printf("Error opening file\n");
+      exit(0);
     }
 
-    //At Client 1
-    //Write the first 200 bytes of data from the input file onto pfs_file
-    err_value = pfs_write(fdes, (void *)buf, 4*ONEKB, 0, &cache_hit);
-    printf("Wrote %d bytes to the file\n", err_value);
-    //sleep(5);
-    //err_value = pfs_read(fdes, (void *)buf, 4*ONEKB, 0, &cache_hit);
-    //fprintf(stderr, "This should be a cache_hit: %d\n", cache_hit);
-    sleep(10);
-    pfs_close(fdes);
-    free(buf);
-    close(ifdes);
-    return 0;
+  //At Client 1
+  //Write the first 200 bytes of data from the input file onto pfs_file
+  err_value = pfs_write(fdes, (void *)buf, 4*ONEKB, 0, &cache_hit);
+  printf("Wrote %d bytes to the file\n", err_value);
+
+  pfs_close(fdes);
+  free(buf); 
+  close(ifdes);
+  return 0;
 }
